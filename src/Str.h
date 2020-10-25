ebeeaeefbc564ef863b814633c72259137b6cb8f
lablnet <mumerfarooqlablnet01@gmail.com>
2020-10-25 17:16:18 +0500

    update split function

--- src/Str.h
+++ src/Str.h
@@ -10,16 +10,22 @@
 
 
 #include <string>
+#include <vector>
+#include <cstring>
 
 #ifndef HECK_STR_H
 #define HECK_STR_H
 namespace Lablnet {
-    std::string split(std::string str, std::string delimiter) {
-        size_t pos = 0;
-        while ((pos = str.find(delimiter)) != std::string::npos) {
-            str.erase(0, pos + delimiter.length());
+    std::vector<std::string> split(std::string& str,const std::string& sep) {
+        char* cstr = const_cast<char*>(str.c_str());
+        char* current;
+        std::vector<std::string> arr;
+        current=strtok(cstr,sep.c_str());
+        while(current!=NULL){
+            arr.push_back(current);
+            current=strtok(NULL,sep.c_str());
         }
-        return str;
+        return arr;
     }
 }
 
